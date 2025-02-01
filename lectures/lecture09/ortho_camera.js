7abb061bb29a1b4dd09d92dc6d61934bf8e1a689
Asa <cyberasasoftware@gmail.com>
2025-02-01 12:24:12 -0600

    change the perspective works

--- lectures/lecture09/ortho_camera.js
+++ lectures/lecture09/ortho_camera.js
@@ -29,8 +29,6 @@ var g_lastFrameMS
 // GLSL uniform references
 var g_u_model_ref
 var g_u_world_ref
-var g_u_camera_ref
-var g_u_projection_ref
 
 // usual model/world matrices
 var g_modelMatrix
@@ -45,16 +43,15 @@ var g_right
 var g_top
 var g_bottom
 
-// camera projection values
-var g_fovy
-var g_aspect
-var g_near
-var g_far
+// the current axis of rotation
+var g_rotationAxis
 
+// Whether or not we have culling and depth enabled
+var g_cull
+var g_depth
 
 // Mesh definitions
 var g_teapotMesh
-var g_gridMesh
 
 // We're using triangles, so our vertices each have 3 elements
 const TRIANGLE_SIZE = 3
@@ -63,8 +60,7 @@ const TRIANGLE_SIZE = 3
 const FLOAT_SIZE = 4
 
 function main() {
-    g_canvas = document.getElementById('canvas')
-
+    // Setup our 6 sliders
     slider_input = document.getElementById('sliderNear')
     slider_input.addEventListener('input', (event) => {
         updateNear(event.target.value)
@@ -95,26 +91,7 @@ function main() {
         updateBottom(event.target.value)
     })
 
-    slider_input = document.getElementById('sliderFOVY')
-    slider_input.addEventListener('input', (event) => {
-        updateFOVY(event.target.value)
-    })
-
-    slider_input = document.getElementById('sliderAspect')
-    slider_input.addEventListener('input', (event) => {
-        updateAspect(event.target.value)
-    })
-
-    slider_input = document.getElementById('sliderNear')
-    slider_input.addEventListener('input', (event) => {
-        updateNear(event.target.value)
-    })
-
-    slider_input = document.getElementById('sliderFar')
-    slider_input.addEventListener('input', (event) => {
-        updateFar(event.target.value)
-    })
-
+    g_canvas = document.getElementById('canvas')
 
     // Get the rendering context for WebGL
     gl = getWebGLContext(g_canvas, true)
@@ -134,7 +111,7 @@ function main() {
  */
 async function loadOBJFiles() {
     // open our OBJ file(s)
-    data = await fetch('./resources/teapot.obj').then(response => response.text()).then((x) => x)
+    data = await fetch('/resources/teapot.obj').then(response => response.text()).then((x) => x)
     g_teapotMesh = []
     readObjFile(data, g_teapotMesh)
 
@@ -150,10 +127,8 @@ function startRendering() {
     }
 
     // initialize the VBO
-    var gridInfo = buildGridAttributes(1, 1, [0.0, 1.0, 0.0])
-    g_gridMesh = gridInfo[0]
-    var teapotColors = buildColorAttributes(g_teapotMesh.length / 3)
-    var data = g_teapotMesh.concat(gridInfo[0]).concat(teapotColors).concat(gridInfo[1])
+    var colors = buildColorAttributes(g_teapotMesh.length / 3)
+    var data = g_teapotMesh.concat(colors)
     if (!initVBO(new Float32Array(data))) {
         return
     }
@@ -162,8 +137,8 @@ function startRendering() {
     if (!setupVec3('a_Position', 0, 0)) {
         return
     }
-    if (!setupVec3('a_Color', 0, (g_teapotMesh.length + gridInfo[0].length) * FLOAT_SIZE)) {
-        return -1
+    if (!setupVec3('a_Color', 0, g_teapotMesh.length * FLOAT_SIZE)) {
+        return
     }
 
     // Get references to GLSL uniforms
@@ -174,30 +149,28 @@ function startRendering() {
 
     // Setup our model by scaling
     g_modelMatrix = new Matrix4()
-    g_modelMatrix = g_modelMatrix.setScale(.005, .005, .005)
+    g_modelMatrix = g_modelMatrix.setScale(.01, .01, -.01)
 
-    // Reposition our mesh (in this case as an identity operation)
-    g_worldMatrix = new Matrix4()
+    // Reposition our mesh to fit in the projection
+    g_worldMatrix = new Matrix4().translate(0, 0, -2)
+
+    // Initially set our camera to be in a default position
     g_cameraMatrix = new Matrix4()
 
-    // Enable culling and depth tests
+    // Enable culling and depth
     gl.enable(gl.CULL_FACE)
     gl.enable(gl.DEPTH_TEST)
 
     // Setup for ticks
     g_lastFrameMS = Date.now()
 
+    // Make sure we have a defined rotation
     updateNear(1)
-    updateFar(-1)
+    updateFar(4)
     updateLeft(-1)
     updateRight(1)
     updateBottom(-1)
     updateTop(1)
-
-    updateFOVY(168)
-    updateAspect(1)
-    updateNear(1)
-    updateFar(4)
 
     tick()
 }
@@ -215,7 +188,7 @@ function tick() {
     deltaTime = current_time - g_lastFrameMS
     g_lastFrameMS = current_time
 
-    // rotate the arm constantly around the given axis (of the model)
+    // rotate the teapot constantly around the Y axis of the model
     angle = ROTATION_SPEED * deltaTime
     g_modelMatrix.concat(new Matrix4().setRotate(angle, 0, 1, 0))
 
@@ -226,8 +199,8 @@ function tick() {
 
 // draw to the screen on the next frame
 function draw() {
-    // var projection_matrix = new Matrix4().setOrtho(g_left, g_right, g_bottom, g_top, g_near, g_far)
-    var projection_matrix = new Matrix4().setPerspective(g_fovy, g_aspect, g_near, g_far)
+    // Construct our orthographic projection matrix
+    var projection_matrix = new Matrix4().setOrtho(g_left, g_right, g_bottom, g_top, g_near, g_far)
 
     // Update with our global transformation matrices
     gl.uniformMatrix4fv(g_u_model_ref, false, g_modelMatrix.elements)
@@ -238,19 +211,7 @@ function draw() {
     // Clear the canvas with a black background
     gl.clearColor(0.0, 0.0, 0.0, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT)
-
-    // draw our one model (the teapot)
     gl.drawArrays(gl.TRIANGLES, 0, g_teapotMesh.length / 3)
-
-    // the grid has a constant identity matrix for model and world
-    // world includes our Y offset
-    gl.uniformMatrix4fv(g_u_model_ref, false, new Matrix4().elements)
-    gl.uniformMatrix4fv(g_u_world_ref, false, new Matrix4().translate(0, GRID_Y_OFFSET, 0).elements)
-
-    // draw the grid
-    gl.drawArrays(gl.LINES, g_teapotMesh.length / 3, g_gridMesh.length / 3)
-//     console.log(g_teapotMesh.length / 3)
-//     console.log(g_gridMesh.length / 3)
 }
 
 // Helper to construct colors
@@ -267,86 +228,6 @@ function buildColorAttributes(vertex_count) {
 
     return colors
 }
-
-// How far in the X and Z directions the grid should extend
-// Recall that the camera "rests" on the X/Z plane, since Z is "out" from the camera
-const GRID_X_RANGE = 1000
-const GRID_Z_RANGE = 1000
-
-// The default y-offset of the grid for rendering
-const GRID_Y_OFFSET = -0.5
-
-/*
- * Helper to build a grid mesh and colors
- * Returns these results as a pair of arrays
- * Each vertex in the mesh is constructed with an associated grid_color
- */
-function buildGridAttributes(grid_row_spacing, grid_column_spacing, grid_color) {
-    var mesh = []
-    var colors = []
-
-    // Construct the rows
-    for (var x = -GRID_X_RANGE; x < GRID_X_RANGE; x += grid_row_spacing) {
-        // two vertices for each line
-        // one at -Z and one at +Z
-        mesh.push(x, 0, -GRID_Z_RANGE)
-        mesh.push(x, 0, GRID_Z_RANGE)
-    }
-
-    // Construct the columns extending "outward" from the camera
-    for (var z = -GRID_Z_RANGE; z < GRID_Z_RANGE; z += grid_column_spacing) {
-        // two vertices for each line
-        // one at -Z and one at +Z
-        mesh.push(-GRID_X_RANGE, 0, z)
-        mesh.push(GRID_X_RANGE, 0, z)
-    }
-
-    // We need one color per vertex
-    // since we have 3 components for each vertex, this is length/3
-    for (var i = 0; i < mesh.length / 3; i++) {
-        colors.push(grid_color[0], grid_color[1], grid_color[2])
-    }
-
-    return [mesh, colors]
-}
-
-/*
- * Initialize the VBO with the provided data
- * Assumes we are going to have "static" (unchanging) data
- */
-function initVBO(data) {
-    // get the VBO handle
-    var VBOloc = gl.createBuffer()
-    if (!VBOloc) {
-        return false
-    }
-
-    // Bind the VBO to the GPU array and copy `data` into that VBO
-    gl.bindBuffer(gl.ARRAY_BUFFER, VBOloc)
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
-
-    return true
-}
-
-/*
- * Helper function to load the given vec3 data chunk onto the VBO
- * Requires that the VBO already be setup and assigned to the GPU
- */
-function setupVec3(name, stride, offset) {
-    // Get the attribute by name
-    var attributeID = gl.getAttribLocation(gl.program, `${name}`)
-    if (attributeID < 0) {
-        console.log(`Failed to get the storage location of ${name}`)
-        return false
-    }
-
-    // Set how the GPU fills the a_Position variable with data from the GPU 
-    gl.vertexAttribPointer(attributeID, 3, gl.FLOAT, false, stride, offset)
-    gl.enableVertexAttribArray(attributeID)
-
-    return true
-}
-
 
 function updateNear(amount) {
     label = document.getElementById('near')
@@ -384,26 +265,40 @@ function updateTop(amount) {
     g_top = Number(amount)
 }
 
-function updateFOVY(amount) {
-    label = document.getElementById('fovy')
-    label.textContent = `FOVY: ${Number(amount).toFixed(2)}`
-    g_fovy = Number(amount)
+/*
+ * Initialize the VBO with the provided data
+ * Assumes we are going to have "static" (unchanging) data
+ */
+function initVBO(data) {
+    // get the VBO handle
+    var VBOloc = gl.createBuffer()
+    if (!VBOloc) {
+        console.log('Failed to create the vertex buffer object')
+        return false
+    }
+
+    // Bind the VBO to the GPU array and copy `data` into that VBO
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBOloc)
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
+
+    return true
 }
 
-function updateAspect(amount) {
-    label = document.getElementById('aspect')
-    label.textContent = `Aspect: ${Number(amount).toFixed(2)}`
-    g_aspect = Number(amount)
-}
+/*
+ * Helper function to load the given vec3 data chunk onto the VBO
+ * Requires that the VBO already be setup and assigned to the GPU
+ */
+function setupVec3(name, stride, offset) {
+    // Get the attribute by name
+    var attributeID = gl.getAttribLocation(gl.program, `${name}`)
+    if (attributeID < 0) {
+        console.log(`Failed to get the storage location of ${name}`)
+        return false
+    }
 
-function updateNear(amount) {
-    label = document.getElementById('near')
-    label.textContent = `Near: ${Number(amount).toFixed(2)}`
-    g_near = Number(amount)
-}
+    // Set how the GPU fills the a_Position variable with data from the GPU 
+    gl.vertexAttribPointer(attributeID, 3, gl.FLOAT, false, stride, offset)
+    gl.enableVertexAttribArray(attributeID)
 
-function updateFar(amount) {
-    label = document.getElementById('far')
-    label.textContent = `Far: ${Number(amount).toFixed(2)}`
-    g_far = Number(amount)
+    return true
 }
